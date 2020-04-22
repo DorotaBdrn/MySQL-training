9271f5317c34648abe591f35b0dab64a3eab27fe
DorotaBdrn <dorotabiedron@icloud.com>
2020-04-22 12:21:32 +0200

    JDBC- MySQL connected

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,7 @@
 package com.company;
 
 import java.sql.*;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Main {
 
@@ -16,10 +16,6 @@ public class Main {
     static String password = "root";
 
 
-
-
-
-
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
 
 
@@ -30,29 +26,16 @@ public class Main {
         selectData(stmt);
 
 
-//        int status = ps.executeUpdate();
-//
-//        if (status != 0) {
-//            System.out.println("Database was connected ");
-//            System.out.println("Record was inserted");
-
-//        }
-
-
-//        BD2 data = new BD2();
-//        data.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        data.setSize(275,180);
-//        data.setVisible(true);
-
+        new DBclass().setVisible(true);
 
     }
+
     public static void selectData(Statement stmt) throws SQLException {
         ResultSet resultSet = stmt.executeQuery("SELECT * from Student");
-        while (resultSet.next()){
-            String name =resultSet.getString( "name");
+        while (resultSet.next()) {
+            String name = resultSet.getString("name");
             int id = resultSet.getInt("idStudent");
-            System.out.println("id: " + id + " name: "+ name);
-
+            System.out.println("id: " + id + " name: " + name);
         }
     }
 }
